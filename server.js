6c92317149a5507270c58aaf3445ef127386d4c5
thinesh <thinesh12@gmail.com>
2023-07-04 23:43:52 -0400

    server.js

--- server.js
+++ server.js
@@ -1,58 +1,17 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+const middlewares = jsonServer.defaults();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
 
-const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-
-app.get('/movies', (req, res) => {
-  const movies = JSON.parse(fs.readFileSync('db.json')).movies;
-  res.json(movies);
-});
-
-app.get('/movies/:id', (req, res) => {
-  const movies = JSON.parse(fs.readFileSync('db.json')).movies;
-  const movie = movies.find((m) => m.id === parseInt(req.params.id));
-
-  if (movie) {
-    res.json(movie);
-  } else {
-    res.status(404).json({ error: 'Movie not found' });
-  }
-});
-
-app.post('/movies', (req, res) => {
-  const movies = JSON.parse(fs.readFileSync('db.json')).movies;
-  const newMovie = {
-    id: movies.length + 1,
-    title: req.body.title,
-    description: req.body.description,
-  };
-
-  movies.push(newMovie);
-  fs.writeFileSync('db.json', JSON.stringify({ movies }));
-
-  res.json(newMovie);
-});
-
-app.put('/movies/:id', (req, res) => {
-  const movies = JSON.parse(fs.readFileSync('db.json')).movies;
-  const movie = movies.find((m) => m.id === parseInt(req.params.id));
-
-  if (movie) {
-    movie.title = req.body.title;
-    movie.description = req.body.description;
-    fs.writeFileSync('db.json', JSON.stringify({ movies }));
-
-    res.json(movie);
-  } else {
-    res.status(404).json({ error: 'Movie not found' });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
